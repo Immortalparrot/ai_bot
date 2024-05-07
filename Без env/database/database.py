070b1e3cf44148a19f4f7334ed51e268e9fa1221
yuliaat1412 <yuliaatroshenkova@yandex.ru>
2024-05-07 19:10:10 +0300

    Create database.py

---
+++ Без env/database/database.py
@@ -0,0 +1,10 @@
+user_dict_template: dict = {
+  'kb_number': 0,
+  'curent_simptoms': set(),
+  'curent_diagnoses': None,
+  'language': 'english',
+  'diagnose': False,
+  'histori': dict()
+}
+
+users_db: dict = {}
